72f70d0f3c0f31dfd0fe1a8f33090bfa759e6b6b
RupeshDarpe <darperupesh17@gmail.com>
2024-08-05 11:40:59 +0530

    ABC

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId='d40cd735-27f1-46e3-b771-ad1edb82d739'
+        username={props.user.username} // adam
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
